cfdef8fe46e9748814a66e5632088ab7ce8fbfe5
lifenglei <lifenglei@360.cn>
2022-05-19 13:44:32 +0800

    docs: 添加eslint校验

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 const App = () => <div>hello parcel created by lifenglei 增加eslint</div>;
-
-function show() {
-  let sss2 = "name";
+function show () {
+  let sss2 = 'name';
   console.log(sss2);
 }
-
 show();
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(<App />, document.getElementById('app'));
