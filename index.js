3d2fb2e04b4bf31c95c4330bd2b6862fc273d055
lifenglei <lifenglei@360.cn>
2022-05-19 11:19:26 +0800

    docs: add x permiss. to file, otherwise it will not run

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const App = () => (
   <div>hello parcel created by lifenglei 增加eslint</div>
   
 )
+
+function show() {
+  console.log(sss)
+}
+
+show()
 ReactDom.render(<App/>,document.getElementById('app'))
