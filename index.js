e04e3254112b83fa9df84375736af66c15377eff
lifenglei <lifenglei@360.cn>
2022-05-19 10:18:00 +0800

    fix: 修改文案

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 const App = () => (
-  <div>hello parcel created by lifenglei</div>
+  <div>hello parcel created by lifenglei 增加eslint</div>
+  
 )
 ReactDom.render(<App/>,document.getElementById('app'))
